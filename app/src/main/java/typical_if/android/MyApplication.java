449b896d27821c5e5872f37577b17d2c7ffe5022
GIGAMOLE <gigamole53@gmail.com>
2014-07-31 11:00:36 +0300

    Видалив опції для імеджлоадера.

--- app/src/main/java/typical_if/android/MyApplication.java
+++ app/src/main/java/typical_if/android/MyApplication.java
@@ -3,8 +3,6 @@ package typical_if.android;
 import android.app.Application;
 import android.util.DisplayMetrics;
 
-import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -28,10 +26,9 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        final DisplayMetrics displayMetrics =  getApplicationContext().getResources().getDisplayMetrics();
-        //int displayDensity = displayMetrics.densityDpi;
+        final DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
         displayHeight = displayMetrics.heightPixels;
-        displayWidth =  displayMetrics.widthPixels;
+        displayWidth = displayMetrics.widthPixels;
 
 
         // Create global configuration and initialize ImageLoader with this configuration
@@ -40,35 +37,23 @@ public class MyApplication extends Application {
                 .diskCacheExtraOptions(480, 800, null)
                         //.taskExecutor(...)
                         //.taskExecutorForCachedImages(...)
-                .threadPoolSize(10) // default
-                .threadPriority(Thread.NORM_PRIORITY - 2) // default
-//                .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+                        //.threadPoolSize(3) // default
+                        //.threadPriority(Thread.NORM_PRIORITY - 1) // default
+                        //.tasksProcessingOrder(QueueProcessingType.FIFO) // default
                 .denyCacheImageMultipleSizesInMemory()
-                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
-                .memoryCacheSize(2 * 1024 * 1024)
-//                .memoryCacheSizePercentage(13) // default
-//                .diskCache(new UnlimitedDiscCache(cacheDir)) // default
-                .diskCacheSize(50 * 1024 * 1024)
-                .diskCacheFileCount(100)
-//                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
-//                .imageDownloader(new BaseImageDownloader(VKUIHelper.getApplicationContext())) // default
-//                .imageDecoder(new BaseImageDecoder()) // default
-                .defaultDisplayImageOptions(getDisplayOptions()) // default
+                        //.memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                        //.memoryCacheSize(2 * 1024 * 1024)
+                        //.memoryCacheSizePercentage(13) // default
+                        //.diskCache(new UnlimitedDiscCache(cacheDir)) // default
+                        //.diskCacheSize(50 * 1024 * 1024)
+                        //.diskCacheFileCount(100)
+                        //.diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+                        //.imageDownloader(new BaseImageDownloader(context)) // default
+                        //.imageDecoder(new BaseImageDecoder()) // default
+                        //.defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
                 .writeDebugLogs()
                 .build();
         ImageLoader.getInstance().init(config);
 
-
-    }
-    public static DisplayImageOptions getDisplayOptions() {
-        DisplayImageOptions options = new DisplayImageOptions.Builder()
-                //.showImageForEmptyUri(R.drawable.error)
-                //.showImageOnFail(R.drawable.error)
-                .delayBeforeLoading(1000)
-                .resetViewBeforeLoading(false)  // default
-                .cacheInMemory(true) // default
-                .cacheOnDisc(true) // default
-                .build();
-        return options;
     }
 }
