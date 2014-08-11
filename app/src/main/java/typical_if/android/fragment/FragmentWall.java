7d3f773b2047187517120ddeef695fc9da4e16af
SokeOner <SokeOner9@gmail.com>
2014-08-11 19:24:30 +0300

    Full push to refresh new

--- app/src/main/java/typical_if/android/fragment/FragmentWall.java
+++ app/src/main/java/typical_if/android/fragment/FragmentWall.java
@@ -2,11 +2,15 @@ package typical_if.android.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.RelativeLayout;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -19,6 +23,7 @@ import com.vk.sdk.api.VKResponse;
 import org.json.JSONObject;
 
 import typical_if.android.ItemDataSetter;
+import typical_if.android.OfflineMode;
 import typical_if.android.R;
 import typical_if.android.VKHelper;
 import typical_if.android.adapter.WallAdapter;
@@ -30,23 +35,24 @@ import typical_if.android.model.Wall.Wall;
  */
 
 
-public class FragmentWall extends Fragment {
+public class FragmentWall extends Fragment implements AbsListView.OnScrollListener, SwipeRefreshLayout.OnRefreshListener {
     private static final String ARG_VK_GROUP_ID = "vk_group_id";
-    final String SAVED_TEXT = "saved_text";
+    //final String SAVED_TEXT = "saved_text";
 
-    private int mCurrentTransitionEffect = JazzyHelper.TILT;
+    private int mCurrentTransitionEffect = JazzyHelper.TRANSPARENT;
     JazzyListView wallListView;
     WallAdapter adapter;
 
     RelativeLayout spinnerLayout;
     View rootView;
+    LayoutInflater inflaterGlobal;
 
     String postColor;
     Long gid;
 
+    SwipeRefreshLayout swipeView;
+    AbsListView.OnScrollListener onScrollListenerObject;
     Bundle arguments;
-    //SharedPreferences sPref;
-    //JSONObject jsonObj;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -67,37 +73,22 @@ public class FragmentWall extends Fragment {
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_wall, container, false);
         spinnerLayout = (RelativeLayout) rootView.findViewById(R.id.spinner_layout);
+        inflaterGlobal = inflater;
 
         arguments = getArguments();
         gid = arguments.getLong(ARG_VK_GROUP_ID);
         postColor = ItemDataSetter.getPostColor(gid);
 
-        VKHelper.doGroupWallRequest(gid, new VKRequest.VKRequestListener() {
-            public void onComplete(VKResponse response) {
-                super.onComplete(response);
-                initGroupWall(response.json, inflater);
-                spinnerLayout.setVisibility(View.GONE);
-            }
-/*
-            @Override
-            public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
-                super.attemptFailed(request, attemptNumber, totalAttempts);
-                    initGroupWall(loadJSON(), inflater, gid);
-                    spinnerLayout.setVisibility(View.GONE);
-            }
 
-            @Override
-            public void onError(VKError error) {
-                super.onError(error);
-                initGroupWall(loadJSON(), inflater, gid);
-                spinnerLayout.setVisibility(View.GONE);
-            }
+       // wallListView.setOnScrollListener(this);
+        onScrollListenerObject = this;
+        swipeView = (SwipeRefreshLayout) rootView.findViewById(R.id.refresh);
+        swipeView.setOnRefreshListener(this);
+        swipeView.setColorScheme(android.R.color.holo_blue_dark, android.R.color.holo_blue_light, android.R.color.holo_green_light, android.R.color.holo_green_light);
+        initGroupWall(OfflineMode.loadJSON(gid), inflater);
+        spinnerLayout.setVisibility(View.GONE);
+        wallListView.setOnScrollListener(this);
 
-            @Override
-            public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded, long bytesTotal) {
-                super.onProgress(progressType, bytesLoaded, bytesTotal);
-            }*/
-        });
 
         return rootView;
     }
@@ -115,6 +106,45 @@ public class FragmentWall extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        //((MainActivity) activity).onSectionAttached(getArguments().getLong(ARG_VK_GROUP_ID));
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int i) {
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int i, int i2, int i3) {
+        boolean enable = false;
+        if (absListView != null && absListView.getChildCount() > 0) {
+            // check if the first item of the list is visible
+            boolean firstItemVisible = absListView.getFirstVisiblePosition() == 0;
+            // check if the top of the first item is visible
+            boolean topOfFirstItemVisible = absListView.getChildAt(0).getTop() == 0;
+            // enabling or disabling the refresh layout
+            enable = firstItemVisible && topOfFirstItemVisible;
+        }
+        swipeView.setEnabled(enable);
+    }
+
+    @Override
+    public void onRefresh() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeView.setRefreshing(false);
+                VKHelper.doGroupWallRequest(gid, new VKRequest.VKRequestListener() {
+                    @Override
+                    //jhghgf
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+                        OfflineMode.saveJSON(response.json, gid);
+                        initGroupWall(OfflineMode.loadJSON(gid), inflaterGlobal);
+                        wallListView.setOnScrollListener(onScrollListenerObject);
+                    }
+                });
+            }
+        }, 1000);
+
     }
 }
