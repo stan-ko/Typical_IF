ca5855b6b8edd859a3a46797190dc94e4b5e815c
Stan <koshutsky@rambler.ru>
2014-08-14 15:01:32 +0300

    SplashActivity без таймера, с проверкой инета и стартом MainActivity после обработки 4х ВК-запросов

--- app/src/main/java/typical_if/android/activity/SplashActivity.java
+++ app/src/main/java/typical_if/android/activity/SplashActivity.java
@@ -2,10 +2,10 @@ package typical_if.android.activity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +24,7 @@ import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
 
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import typical_if.android.Constants;
 import typical_if.android.ItemDataSetter;
@@ -56,17 +57,17 @@ public class SplashActivity extends Activity implements Animation.AnimationListe
         textView.startAnimation(animMoveDown);
 
         animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-        animFadeIn.setAnimationListener(this);
+        //animFadeIn.setAnimationListener(this);
         imageView.startAnimation(animFadeIn);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, 5000);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+//                startActivity(i);
+//                finish();
+//            }
+//        }, 5000);
 
         locale = new Locale("uk");
         Locale.setDefault(locale);
@@ -79,58 +80,109 @@ public class SplashActivity extends Activity implements Animation.AnimationListe
         VKSdk.initialize(sdkListener, Constants.APP_ID, VKAccessToken.tokenFromSharedPreferences(this, sTokenKey));
 
         ItemDataSetter.loadUserId();
-        if (OfflineMode.isOnline(getApplicationContext())==true) {
-            //   --------------------START------------- all Request from internet before start APP----------------------
-            VKHelper.doGroupWallRequest(Constants.TF_ID, new VKRequest.VKRequestListener() {
-                @Override
-                public void onComplete(VKResponse response) {
-                    super.onComplete(response);
-                    OfflineMode.saveJSON(response.json, Constants.TF_ID);
-                }
-            });
-            VKHelper.doGroupWallRequest(Constants.TZ_ID, new VKRequest.VKRequestListener() {
-                @Override
-                public void onComplete(VKResponse response) {
-                    super.onComplete(response);
-                    OfflineMode.saveJSON(response.json, Constants.TZ_ID);
-                }
-            });
-            VKHelper.doGroupWallRequest(Constants.FB_ID, new VKRequest.VKRequestListener() {
-                @Override
-                public void onComplete(VKResponse response) {
-                    super.onComplete(response);
-                    OfflineMode.saveJSON(response.json, Constants.FB_ID);
-                }
-            });
-            VKHelper.doGroupWallRequest(Constants.FN_ID, new VKRequest.VKRequestListener() {
-                @Override
-                public void onComplete(VKResponse response) {
-                    super.onComplete(response);
-                    OfflineMode.saveJSON(response.json, Constants.FN_ID);
-                }
-            });
-            //-------------------------END-------- all Request from internet before start APP----------------------
-        }else{
-            Log.d("----------------Internet conection Error","------------------------");
-
-//            Thread t = new Thread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(VKUIHelper.getTopActivity());
-//                    builder.setMessage("dialog_fire_missiles")
-//                            .setPositiveButton("yes", new DialogInterface.OnClickListener() {
-//                                public void onClick(DialogInterface dialog, int id) {
-//                                }
-//                            })
-//                            .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-//                                public void onClick(DialogInterface dialog, int id) {
-//                                    // User cancelled the dialog
-//                                }
-//                            });
-//                    builder.create().show();}
-//            });
-        };
     }
+
+    void showAlertNoInternet() {
+        Log.d("----------------Internet conection Error", "------------------------");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.app_name)
+                .setMessage("No fucking active Internet connection is available. Would you like to")
+                .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        checkIfOnlineAndProceed();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        // TODO: add neutral button if has cached data to work offline with
+
+        builder.create().show();
+    }
+
+    void checkIfOnlineAndProceed() {
+        if ( OfflineMode.isOnline(getApplicationContext()) ) {
+            makeRequests();
+        } else {
+            showAlertNoInternet();
+        }
+    }
+
+    private void makeRequests(){
+        threadsCounter = new AtomicInteger(4);
+        //   --------------------START------------- all Request from internet before start APP----------------------
+        VKHelper.doGroupWallRequest(Constants.TF_ID, new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                OfflineMode.saveJSON(response.json, Constants.TF_ID);
+                decrementThreadsCounter();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                super.onError(error);
+                decrementThreadsCounter();
+            }
+        });
+        VKHelper.doGroupWallRequest(Constants.TZ_ID, new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                OfflineMode.saveJSON(response.json, Constants.TZ_ID);
+                decrementThreadsCounter();
+            }
+            @Override
+            public void onError(VKError error) {
+                super.onError(error);
+                decrementThreadsCounter();
+            }
+        });
+        VKHelper.doGroupWallRequest(Constants.FB_ID, new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                OfflineMode.saveJSON(response.json, Constants.FB_ID);
+                decrementThreadsCounter();
+            }
+            @Override
+            public void onError(VKError error) {
+                super.onError(error);
+                decrementThreadsCounter();
+            }
+        });
+        VKHelper.doGroupWallRequest(Constants.FN_ID, new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                OfflineMode.saveJSON(response.json, Constants.FN_ID);
+                decrementThreadsCounter();
+            }
+            @Override
+            public void onError(VKError error) {
+                super.onError(error);
+                decrementThreadsCounter();
+            }
+        });
+        //-------------------------END-------- all Request from internet before start APP----------------------
+    }
+
+    AtomicInteger threadsCounter;
+    void decrementThreadsCounter(){
+        if (threadsCounter.decrementAndGet()==0)
+            startNextActivity();
+    }
+
+    private void startNextActivity() {
+        final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
     private final VKSdkListener sdkListener = new VKSdkListener() {
         @Override
         public void onCaptchaError(VKError captchaError) {
@@ -174,18 +226,15 @@ public class SplashActivity extends Activity implements Animation.AnimationListe
         return super.onOptionsItemSelected(item);
     }
 
+    // Animation listener implementation
     @Override
-    public void onAnimationStart(Animation animation) {
-
-    }
+    public void onAnimationStart(Animation animation) {}
 
     @Override
     public void onAnimationEnd(Animation animation) {
-
+        checkIfOnlineAndProceed();
     }
 
     @Override
-    public void onAnimationRepeat(Animation animation) {
-
-    }
+    public void onAnimationRepeat(Animation animation) {}
 }
